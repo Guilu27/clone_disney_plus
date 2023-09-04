ef3b5bf49d6aacbe9df3367e47a03143047d7044
Guilherme <guilherme@exemplo.com>
2023-09-04 17:55:01 -0300

    header feito

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("[data-tab-button]");
   const questions = document.querySelectorAll("[data-faq-question]");
 
+  const heroSection = document.querySelector(".hero");
+  const heroHeight = heroSection.clientHeight;
+
+  window.addEventListener("scroll", function () {
+    const currentPosition = window.scrollY;
+
+    if (currentPosition < heroHeight) {
+      hiddenHeaderElements();
+    } else {
+      showsHeaderElements();
+    }
+  });
+
+  // Tabs de programacoes
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (button) {
       const tabTarget = button.target.dataset.tabButton;
@@ -13,10 +27,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  // Accordion secao FAQ
   for (let i = 0; i < questions.length; i++) {
     questions[i].addEventListener("click", openOrCloseQuestion);
   }
 });
+
+function hiddenHeaderElements() {
+  const header = document.querySelector("header");
+  header.classList.add("header--is-hidden");
+}
+
+function showsHeaderElements() {
+  const header = document.querySelector("header");
+  header.classList.remove("header--is-hidden");
+}
 
 function openOrCloseQuestion(e) {
   const className = "faq__questions__item--is-open";
